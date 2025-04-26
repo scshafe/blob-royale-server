d5c2bd4beae1855b34704134324654c3b05ad117
scshafe <scshafe@umich.edu>
2025-04-26 03:06:47 -0500

    player struct

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,43 @@
+
+
+#[derive(Debug)]
+struct Point {
+    x: i32,
+    y: i32,
+}
+
+#[derive(Debug)]
+struct Vector {
+    x: i32,
+    y: i32,
+}
+
+
+#[derive(Debug)]
+struct Player {
+    center: Point,
+    radius: i8,
+    velocity: Vector,
+    acceleration: Vector,
+}
+
+#[derive(Debug)]
+struct GameMap {
+    height: i32,
+    width: i32,
+}    
+
+
+
 fn main() {
-    println!("Hello, world!");
+    println!("yeet");
+
+    let p1: Player = Player {
+        center: Point { x: 10, y: 10 },
+        radius: 5,
+        velocity: Vector { x: 3, y: 1},
+        acceleration: Vector { x: 1, y: -1},
+    };
+
+    println!("{:?}", p1);
 }
